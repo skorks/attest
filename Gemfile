a7a7c2f0fb944eb86dc6f8f541c23ae53026b864
Alan Skorkin <alan@skorks.com>
2010-11-08 00:38:00 +1100

    playing around with dependency resolution with bundler and jeweler

--- Gemfile
+++ Gemfile
@@ -1,2 +1,2 @@
-source "http://rubygems.org"
+source :rubygems
 gem "trollop"
--- lib/attest.rb
+++ lib/attest.rb
@@ -1,2 +1,10 @@
+require "rubygems"
+require "bundler"
+Bundler.setup(:default)
+Bundler.require
+
 module Attest
+
 end
+
+puts "HELLO"
