898a8d352a184868c59e994e4646ac5238b6e152
Alan Skorkin <alan@skorks.com>
2010-11-08 18:55:31 +1100

    playing around with code, got a basic viable framework going, need to clean up and then branch

--- lib/attest.rb
+++ lib/attest.rb
@@ -4,49 +4,61 @@ Bundler.setup(:default)
 Bundler.require
 
 module Attest
+  module Dsl
+    def before_all(&block)
+      @before = block
+    end
 
+    def after_all(&block)
+      @after = block
+    end
+
+    def test(description, &block)
+      @tests = {} if !@tests
+      @tests[description] = block
+    end
+  end
+
+  class Itself
+    def initialize(object)
+      @itself = object
+    end
+
+    def should
+     @itself 
+    end
+  end
+
+  class TestContext
+    include Dsl
+    def initialize(description, block)
+      @description = description
+      @block = block
+    end
+
+    def execute
+      self.instance_eval(&@block)
+      puts @description
+      @tests.each_pair do |key, value|
+        @before.call if @before
+        result = value.call
+        puts " - #{key} #{'FAIL' if !result}"
+        @after.call if @after
+      end
+    end
+  end
 end
 
 module Kernel
   private
   def this_tests(subject, &block)
-    puts "this test"
-    execution_environment = ExecutionEnvironment.new(block)
-    execution_environment.evaluate
-    execution_environment.execute
+    test_context = Attest::TestContext.new(subject, block)
+    test_context.execute
   end
 end
 
-class ExecutionEnvironment
-  def initialize(test_definitions_block)
-    @test_definitions_block = test_definitions_block
-    @before = nil
-    @after = nil
-    @tests = {}
-  end
-
-  def evaluate
-    self.instance_eval(&@test_definitions_block)
-  end
-
-  def execute
-    @tests.each_pair do |key, value|
-      puts key
-    end
-  end
-
-  def before_all(&block)
-    puts "before all"
-    @before = block
-  end
-
-  def after_all(&block)
-    puts "after all"
-    @after = block
-  end
-
-  def test(description, &block)
-    puts "test"
-    @tests[description] = block
+class Object
+  def itself 
+    Attest::Itself.new(self)
   end
 end
