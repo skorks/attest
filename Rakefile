76087d976d8a61edc287d71b6b36bbd2213ab471
Alan Skorkin <alan@skorks.com>
2010-12-20 17:18:15 +1100

    added fakefs as dev dependency, possible test configurator tested

--- Rakefile
+++ Rakefile
@@ -12,6 +12,7 @@ begin
     gem.authors = ["Alan Skorkin"]
     #gem.add_runtime_dependency "bundler"
     #gem.add_runtime_dependency "mocha"
+    gem.add_development_dependency "fakefs"
     # gem is a Gem::Specification... see http://www.rubygems.org/read/chapter/20 for additional settings
   end
   Jeweler::GemcutterTasks.new
--- spec/interface/possible_tests_configurator_test.rb
+++ spec/interface/possible_tests_configurator_test.rb
@@ -1,32 +1,55 @@
 require 'attest/interface/possible_tests_configurator'
+require 'fakefs/safe'
 
 this_tests "possible tests configurator" do 
+
+  before_all do 
+    FakeFS.activate!
+    FakeFS::FileSystem.clear
+    @base_test_path = "/path/to/tests"
+    @sub_test_dir_name = "sub_test_dir"
+    @sub_test_dir_path = "#{@base_test_path}/#{@sub_test_dir_name}"
+    @test_file_names = ["file1.rb","file2.rb","file3.rb"]
+    @test_file1_path = "#{@base_test_path}/#{@test_file_names[0]}"
+    @test_file2_path = "#{@sub_test_dir_path}/#{@test_file_names[1]}"
+    @test_file3_path = "#{@sub_test_dir_path}/#{@test_file_names[2]}"
+    FileUtils.mkdir_p(@base_test_path)
+    FileUtils.mkdir_p(@sub_test_dir_path)
+    File.open(@test_file1_path, 'w') {|f| f.write("require 'pp'")}
+    File.open(@test_file2_path, 'w') {|f| f.write("require 'pp'")}
+    File.open(@test_file3_path, 'w') {|f| f.write("require 'pp'")}
+  end
+
+  after_all do 
+    FakeFS.deactivate!
+  end
+
   test("raises exception if no included test locations") do 
     should_raise {PossibleTestsConfigurator.configure(nil)}
   end
 
-  test("should return one location simulating passing in one file") do 
-    included_location = ["file1.rb"]
-    expected_included_location = ["expanded/file1.rb"]
-    File.stubs(:expand_path).returns(expected_included_location[0])
-    PossibleTestsConfigurator.stubs(:file_list_from_single).returns(expected_included_location[0])
-    actual_included_location = PossibleTestsConfigurator.configure(included_location)
-    should_equal(expected_included_location, actual_included_location)
+  test("should return one location when single file is passed in") do 
+    included_locations = [@test_file1_path]
+    actual_included_location = PossibleTestsConfigurator.configure(included_locations)
+    should_equal([@test_file1_path], actual_included_location)
   end
 
-  test("should return two locations simulating passing in one directory with two files") do 
-    included_location = ["dir1"]
-    expanded_dir_location = "expanded/dir1"
-    expected_included_locations = ["expanded/file1.rb", "expanded/file2.rb"]
-    File.expects(:expand_path).with(included_location[0]).returns(expanded_dir_location)
-    PossibleTestsConfigurator.stubs(:file_list_from_single).returns(expected_included_locations)
-    actual_included_locations = PossibleTestsConfigurator.configure(included_location)
-    should_equal(expected_included_locations, actual_included_locations)
+  test("should return two locations when directory with two files is passed in") do 
+    included_locations = [@sub_test_dir_path]
+    actual_included_locations = PossibleTestsConfigurator.configure(included_locations)
+    should_equal([@test_file2_path, @test_file3_path], actual_included_locations)
   end
 
-  test("should return three locations passing in one file and one directory with two files") do 
+  test("should return three locations when single file and dir with two files is passed in") do 
+    included_locations = [@sub_test_dir_path, @test_file1_path]
+    actual_included_locations = PossibleTestsConfigurator.configure(included_locations)
+    should_equal([@test_file2_path, @test_file3_path, @test_file1_path], actual_included_locations)
   end
 
   test("should return two locations passing in one file and one dir with 2 files one of which is excluded") do 
+    included_locations = [@sub_test_dir_path, @test_file1_path]
+    excluded_locations = [@test_file2_path]
+    actual_included_locations = PossibleTestsConfigurator.configure(included_locations, excluded_locations)
+    should_equal([@test_file3_path, @test_file1_path], actual_included_locations)
   end
 end
